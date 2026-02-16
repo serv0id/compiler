ab3670fefd63570a2d5246f0481eb8a66df6b8b3
serv0id <113516593+serv0id@users.noreply.github.com>
2026-02-16 19:28:55 +0530

    add: baby steps

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # compiler
-An exercise building a simple compiler. https://craftinginterpreters.com/contents.html as reference.
+An exercise building a simple compiler. https://craftinginterpreters.com/contents.html first part as reference.
 
 ## notes
 - Uses C instead of Java
 - Understand compilers better to approach binary analysis
+- REPL mode not implemented
--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <utility>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+class Lox {
+public:
+    Lox(const std::string &file_name) {
+        this->file = file_name;
+    }
+    int run();
+    std::string read_file(std::string& file_name);
+
+private:
+    std::string file;
+};
+
+int main(const int argc, char *argv[]) {
+    std::cout << "Welcome to lox compiler." << std::endl;
+
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        exit(64);  // sysexits.h
+    } else {
+        std::string file_name = argv[1];
+        Lox lox = Lox(file_name);
+    }
+
+    return 0;
 }
